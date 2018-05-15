4038173ff9a68a78b0c252cf84095af60a8b12d5
Kathy <kleboeuf@townisp.com>
2018-05-15 17:13:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ var mixedcase = 'Hi there!'
   
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()){
-  return 'I can\'t hear you';
+  return 'I can\'t hear you!';
 } else if (string === string.toUpperCase()) {
   return 'YES INDEED!';
 } else {
- return 'I love you, too';
+ return 'I love you, too!';
 } 
 }
 sayHiToGrandma('hello');
